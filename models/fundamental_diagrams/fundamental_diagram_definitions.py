852758bb29d4f6f7749c257cd19c40002bc28dfa
YannisZa <yzachos@gmail.com>
2021-05-11 20:39:37 +0300

    started smulders experiment tuning

--- models/fundamental_diagrams/fundamental_diagram_definitions.py
+++ models/fundamental_diagrams/fundamental_diagram_definitions.py
@@ -211,16 +211,16 @@ class SmuldersFD(FundamentalDiagram):
         FundamentalDiagram.parameter_names.fset(self, [r'$v_f$',r'$\rho_c$',r'$\rho_j$',r'$\gamma$',r'$\sigma^2$'])
 
     def simulate(self,p):
-        return np.array([ p[0]*r*(1-r/p[2]) if r < p[1] else p[3]*r*(1./r-1./p[2]) for r in super().rho])
+        return p[0]*super().rho*(1-super().rho/p[2]) * ((super().rho < p[1])*1) +  p[3]*super().rho*(1./super().rho-1./p[2]) * ((super().rho >= p[1])*1)
 
     def log_simulate(self,p):
-        return np.array([ np.log(p[0])+np.log(r)+np.log(1-r/p[2]) if r < p[1] else np.log(p[3])+np.log(r)+np.log(1/r-1/p[2]) for r in super().rho])
+        return (np.log(p[0])+np.log(super().rho)+np.log(1-super().rho/p[2])) * ((super().rho < p[1])*1) + (np.log(p[3])+np.log(super().rho)+np.log(1/super().rho-1/p[2])) * ((super().rho >= p[1])*1)
 
     def log_simulate_with_x(self,p,rho):
-        return np.array([ np.log(p[0])+np.log(r)+np.log(1-r/p[2]) if r < p[1] else np.log(p[3])+np.log(r)+np.log(1/r-1/p[2]) for r in rho])
+        return (np.log(p[0])+np.log(rho)+np.log(1-rho/p[2])) * ((rho < p[1])*1) + (np.log(p[3])+np.log(rho)+np.log(1/rho-1/p[2])) * ((rho >= p[1])*1)
 
     def hessian(self,p):
-        return np.array([ -2*(p[0]/p[2]) if r < p[1] else 0 for r in super().rho])
+        return -2*(p[0]/p[2]) * ((super().rho < p[1])*1)
 
 class DeRomphsFD(FundamentalDiagram):
     pass
@@ -232,13 +232,16 @@ class DeRomphsFD(FundamentalDiagram):
         FundamentalDiagram.parameter_names.fset(self, [r'$v_f$',r'$\rho_c$',r'$\rho_j$',r'$\gamma$',r'$\alpha$',r'$\beta$',r'$\sigma^2$'])
 
     def simulate(self,p):
-        return np.array([ p[0]*r*(1-r/p[4]) if r < p[1] else p[3]*r*(1./r-1./p[2])**p[5] for r in super().rho])
+        return p[0]*super().rho*(1-super().rho/p[4]) * (super().rho < p[1])*1 + p[3]*super().rho*(1./super().rho-1./p[2])**p[5] * (super().rho >= p[1])*1
 
     def log_simulate(self,p):
         return np.array([ np.log(p[0])+np.log(r)+np.log(1-r/p[4]) if r < p[1] else np.log(p[3])+np.log(r)+p[5]*np.log(1./r-1./p[2]) for r in super().rho])
 
-    def log_simulate_with_x(self,p,rho):
-        return np.array([ np.log(p[0])+np.log(r)+np.log(1-r/p[4]) if r < p[1] else np.log(p[3])+np.log(r)+p[5]*np.log(1./r-1./p[2]) for r in rho])
+    def log_simulate2(self,p):
+        return (np.log(p[0])+np.log(super().rho)+np.nan_to_num(np.log(1-super().rho/p[4]))) * ((super().rho < p[1])*1) + (np.log(p[3])+np.log(super().rho)+p[5]*np.log(1./super().rho-1./p[2])) * ((super().rho >= p[1])*1)
 
+    def log_simulate_with_x(self,p,rho):
+        return (np.log(p[0])+np.log(rho)+np.nan_to_num(np.log(1-rho/p[4]))) *  ((rho < p[1])*1) + (np.log(p[3])+np.log(rho)+p[5]*np.log(1./rho-1./p[2])) * ((rho >= p[1])*1)
+        
     def hessian(self,p):
-        return np.array([ -2*(p[0]/p[4]) if r < p[1] else ( (p[5]-1)*p[5]*p[3]*(p[2])**2*(1./r-1./p[2])**p[5] )/ ( r*(r-p[2])**2 ) for r in super().rho])
+        -2*(p[0]/p[4]) * (super().rho < p[1])*1 + ( (p[5]-1)*p[5]*p[3]*(p[2])**2*(1./super().rho-1./p[2])**p[5] )/ ( super().rho*(super().rho-p[2])**2 ) * (super().rho >= p[1])*1
